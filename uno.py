70ea7d7a9b2f8ac50a6d73136ff894427a99313b
carlortiz <mr.carlortiz@gmail.com>
2021-04-23 16:24:49 -0700

    Continued player and card classes, check notes

--- uno.py
+++ uno.py
@@ -1,7 +1,7 @@
 import random
 
-types = ["number", "skip", "reverse", "draw_two",
-         "wild", "wild_draw_four"]
+types = ["Number", "Skip", "Reverse", "Draw_Two",
+         "Wild", "Wild_Draw_Four"]
 colors = ["red", "yellow", "green", "blue"]
 players = []
 
@@ -9,34 +9,67 @@ players = []
 class Card():
 
     def __init__(self):
-        self.type = None
-        self.color = colors[self.set_color()]
-        self.number = None
+        self.type = self.set_type()
+        self.color = self.set_color()
 
     def set_type(self):
-        type = random.randint(1, 28)
+        random_type = random.randint(1, 28)
+        types_index = 0
 
-        if type <= 20:
-            return
+        if random_type == 21 or random_type == 22:
+            types_index = 1
+        elif random_type == 23 or random_type == 24:
+            types_index = 2
+        elif random_type == 25 or random_type == 26:
+            types_index = 3
+        elif random_type == 27:
+            types_index = 4
+        elif random_type == 28:
+            types_index = 5
+
+        return types[types_index]
 
     def set_color(self):
-        return random.randint(0,3)
+        return colors[random.randint(0,3)]
 
 
 # PLAYER CLASSES
-class Robot_Player():
+class Players():
+
+    def __init__(self):
+        self.cards = []
+
+        players.append(self)
+
+    def draw_card(self):
+        drawn_card = Card()
+        self.cards.append(drawn_card)
+        print(drawn_card.type)
+
+    def receive_skip(self):
+        pass
+
+    def receive_draw_two(self):
+        pass
+        # self.receive_skip()
+
+    def receive_wild_draw_four(self):
+        pass
+        # self.receive_skip()
+
+class Robot_Player(Players):
 
     def __init__(self, name):
+        super().__init__()
         self.name = name
 
         players.append(self)
 
-class Real_Player():
+class Real_Player(Players):
 
     def __init__(self):
+        super().__init__()
         self.name = input("Your Name: ").upper()
-
-        players.append(self)
 
 # GAME STARTS HERE
 
@@ -48,9 +81,12 @@ player_three = Robot_Player("RABBIT")
 # Set Up Game (order of turns, game over status, top card of discard pile)
 game_rotation = "clockwise"
 game_over = False
-top_card = None
+top_card = player_one.draw_card()
 
 # Deal Cards
+for player in players:
+    for card in range(0, 7):
+        player.draw_card()
 
 # Start Game
 
@@ -58,4 +94,4 @@ top_card = None
 
     # Check If Current Player Is Winner
 
-    # Switch Turns
+    # Next Turn
